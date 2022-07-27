bc212431ea451292bca3304ae3d4f53b56a7cc8c
vinidamiani <48492326+vinidamiani@users.noreply.github.com>
2022-07-27 16:50:25 -0300

    Criado branch

--- VuejsApp2/src/App.vue
+++ VuejsApp2/src/App.vue
@@ -4,19 +4,7 @@
         <hr />
         <router-link to="/">Home</router-link>
         &nbsp;
-        <router-link to="/desafio1">Desafio 1</router-link>
-        &nbsp;
         <router-link to="/desafio2">Desafio 2</router-link>
-        &nbsp;
-        <router-link to="/desafio3">Desafio 3</router-link>
-        &nbsp;
-        <router-link to="/desafio4">Desafio 4</router-link>
-        &nbsp;
-        <router-link to="/desafio5">Desafio 5</router-link>
-        &nbsp;
-        <router-link to="/projeto1">Projeto 1</router-link>
-        &nbsp;
-        <router-link to="/game">Game</router-link>
         <hr />
         <router-view></router-view>
     </div>
--- VuejsApp2/src/main.js
+++ VuejsApp2/src/main.js
@@ -1,10 +1,25 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
+const store = createStore({
+    state() {
+        return {
+            count: 0
+        }
+    },
+    mutations: {
+        increment(state) {
+            state.count++
+        }
+    }
+})
+
 createApp(App)
     .use(router)
     .use(VueAxios, axios)
+    .use(store)
     .mount('#app')
